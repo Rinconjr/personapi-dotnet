e9d4672cd97db6f73536f36edc318c41cfbb3c48
Juan Camilo Quintana <jc.quintana@uniandes.edu.co>
2024-10-17 23:08:28 -0500

    Chuzada

--- personapi-dotnet/Models/Entities/Profesion.cs
+++ personapi-dotnet/Models/Entities/Profesion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace personapi_dotnet.Models.Entities;
 
@@ -7,9 +8,10 @@ public partial class Profesion
 {
     public int Id { get; set; }
 
-    public string Nom { get; set; } = null!;
+    public string? Nom { get; set; } = null!;
 
     public string? Des { get; set; }
 
+    [JsonIgnore]
     public virtual ICollection<Estudio> Estudios { get; set; } = new List<Estudio>();
 }
